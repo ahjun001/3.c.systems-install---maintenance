da5fdc9f24fc350bea92de1c435ca04a744b6bef
ahjun001 <todo.ubuntu@gmail.com>
2022-04-15 15:27:28 +0800

    install & update with github repos before install

--- LM_t-Ub_u/install_apps/a_post_system_install.sh
+++ LM_t-Ub_u/install_apps/a_post_system_install.sh
@@ -17,45 +17,6 @@ set -x
 # check if needed
 # ./list_env.sh
 
-# Set local resources directory, if doesn't exist, script will later attempt to download them in
-RESOURCES="$HOME/Documents/Local resources TBU/"
-[ ! -d "$RESOURCES" ] && mkdir -v "$RESOURCES"
-# for machine with only 1 distro    in ~/Documents/Github
-# or for multi distros              in DATA partition  and then create links to ~/Documents/Github
-
-# Get resources for reset\ all\ links.sh
-
-packageNeeded=git
-if ! command -v "$packageNeeded"; then
-    if [ -x "$(command -v apt)" ]; then
-        sudo apt install "$packageNeeded"
-    elif [ -x "$(command -v dnf)" ]; then
-        sudo dnf install "$packageNeeded"
-    else
-        echo "Package manager not supported"
-        exit 1
-    fi
-fi
-
-# ~/Github relevant resources
-GITHUB_DIR="$HOME/Documents/Github/"
-[ ! -d "$GITHUB_DIR" ] && mkdir -v "$GITHUB_DIR"
-
-old_pwd="$(pwd)"
-cd "$GITHUB_DIR" || exit 1
-
-for repo in \
-    '3.c.systems-install-n-maintain' \
-    '3.a.1-linux' \
-    '3.a.2-vsCode'; do
-    if [ ! -d "$GITHUB_DIR""$repo" ] || [ ! -e "$GITHUB_DIR""$repo" ]; then
-        rm -rv "$repo"
-        git clone 'https://github.com/ahjun001/'"$repo"
-    fi
-done
-cd "$old_pwd" || exit 1
-
-set +x && exit
 # create a bash script to launch installed apps afterwards, pin to panel, install plugins if needed
 printf "#!/usr/bin/env bash\n\nprintf 'Launch apps that have been previously installed\nPin to panel\nInstall plugins if needed'\n\n" >./check_n_pin.sh
 chmod +x check_n_pin.sh
@@ -75,6 +36,8 @@ else
     printf 'echo "sign-in to install plugins"\ncode' >>./check_n_pin.sh
 fi
 exit
+
+set +x && exit
 
 # using mintinstall
 
