26da11aa51ee993560bf45f0fef92658175e4ab9
ahjun001 <todo.ubuntu@gmail.com>
2022-04-22 19:35:34 +0800

    removed early use of plasma-discover

--- LM_t-Ub_u/install_apps/00_post_install.sh
+++ LM_t-Ub_u/install_apps/00_post_install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # setup to install from usb or from local partition
-SOURCE_DIR=/run/media/perubu/USB\ STICK/Local\ resources\ TBU/
-SOURCE_DIR=/run/media/perubu/data/Local\ resources\ TBU/
+# SOURCE_DIR='/run/media/perubu/data/Local resources TBU/'
+SOURCE_DIR='/run/media/perubu/USB STICK/Post-install/Local resources TBU/'
 
 # -e to exit on error, -x to echo commands
-set -e
+set -ex
 
 # shellcheck disable=SC3044
 
@@ -15,9 +15,6 @@ set -e
 
 case $ID in
 fedora)
-    PKG_FMT='.rpm'
-    PKG_MGR='dnf'
-
     #dnf flags
     if ! grep -q 'fastestmirror' /etc/dnf/dnf.conf; then
         echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
@@ -26,15 +23,9 @@ fedora)
     fi
     less /etc/dnf/dnf.conf
 
-    # mount data
-    if ! grep -qs '/dev/nvme0n1p10' /proc/mounts; then
-        sudo mkdir "$SOURCE_DIR"
-        sudo mount /dev/nvme0n1p10 "$SOURCE_DIR"
-    fi
-
     # install expressvpn package available in "$SOURCE_DIR"
     if ! command -v expressvpn; then
-        sudo dnf install "$SOURCE_DIR"'expressvpn*.rpm'
+        sudo dnf install "$SOURCE_DIR"'expressvpn'*'.rpm'
         expressvpn activate
         expressvpn connect
     fi
@@ -42,18 +33,31 @@ fedora)
 
     # modify grub2
     G_FILE=/etc/default/grub
-    if ! grep GRUB_SAVEDEFAULT "$G_FILE"; then echo GRUB_SAVEDEFAULT=true | sudo tee -a "$G_FILE"; fi
-    THEME_DIR=/usr/share/grub/themes/
-    [ ! -d "$THEME_DIR"grub2-them-breeze ] && cp -rv "$SOURCE_DIR"themes/grub2-them-breeze "$THEME_DIR"
-    if ! grep GRUB_THEME "$G_FILE"; then echo GRUB_THEME="/usr/share/grub/themes/grub2-them-breeze/breeze/theme.txt" | sudo tee -a "$G_FILE"; fi
-    less "$G_FILE"
-    sudo rm /boot/efi/EFI/fedora/grub.cfg
-    sudo rm /boot/grub2/grub.cfg
-    sudo dnf reinstall shim-* grub2-efi-* grub2-common
-    
-    # modify ftab
+    if ! grep GRUB_SAVEDEFAULT "$G_FILE"; then
+        echo GRUB_SAVEDEFAULT=true | sudo tee -a "$G_FILE"
 
-    # update repositories
+        THEME_DIR=/usr/share/grub/themes/
+        [ ! -d "$THEME_DIR"grub2-theme-breeze ] && sudo cp -rv "$SOURCE_DIR"themes/grub2-theme-breeze "$THEME_DIR"
+        if ! grep GRUB_THEME "$G_FILE"; then echo GRUB_THEME="/usr/share/grub/themes/grub2-theme-breeze/breeze/theme.txt" | sudo tee -a "$G_FILE"; fi
+        less "$G_FILE"
+        sudo cp "$G_FILE" "$G_FILE".bak
+        sudo rm /boot/efi/EFI/fedora/grub.cfg
+        sudo rm /boot/grub2/grub.cfg
+        sudo dnf reinstall shim-* grub2-efi-* grub2-common
+    fi
+
+    # mount data
+    if ! grep -qs /dev/nvme0n1p10 /proc/mounts; then
+        MOUNT_DIR=/run/media/perubu/data
+        [ ! -d "$MOUNT_DIR" ] && sudo mkdir "$MOUNT_DIR"
+        sudo mount /dev/nvme0n1p10 "$MOUNT_DIR"
+        echo '/dev/disk/by-label/data /mnt/data auto nosuid,nodev,nofail,x-gvfs-show 0 0' | sudo tee -a /etc/fstab
+    fi
+    read -r -n 1 -s -p "plasma-discover to be launched, install gnome-disk, with gnome-disk mount partition data on boot, enter any key to proceed ..." 
+
+    # plasma-discover
+
+    # update repositories on data
 
     # install vim
 
@@ -64,8 +68,11 @@ fedora)
     ;;
 
 linuxmint)
+    PKG_FMT='.rmp'
+    PKG_MGR='dnf'
     PKG_FMT='.deb'
     PKG_MGR='apt'
+    echo "$PKG_FMT" "$PKG_MGR"
     ;;
 
 *)
@@ -75,6 +82,3 @@ linuxmint)
 esac
 
 printf '\n\nExiting %s ...\n' "$0"
-
-# dump
-echo "$PKG_FMT $PKG_MGR"
