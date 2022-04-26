e72e0aa7c4bb4750c68e6044897a160e8683abb0
ahjun001 <todo.ubuntu@gmail.com>
2022-04-26 15:49:49 +0800

    15:49

--- 02_code/02_code.sh
+++ 02_code/02_code.sh
@@ -17,37 +17,37 @@ if [ -z ${ID+x} ]; then . /etc/os-release; fi
 if [ -z ${SOURCE_DIR+x} ]; then SOURCE_DIR="$(pwd)"/; fi
 LOCAL_RES_DIR="$SOURCE_DIR"'Local resources TBU/'
 
-# if ! command -v code; then
-case $ID in
-fedora)
-    PKG_FMT='rpm'
-    PKG_MGR='dnf'
-    MY_TERM_COMMAND='konsole'
-    MY_FLAGS='-e'
-    ;;
-linuxmint | ubuntu)
-    PKG_FMT='deb'
-    PKG_MGR='apt'
-    MY_TERM_COMMAND='gnome-terminal'
-    MY_FLAGS='-x'
-    ;;
-*)
-    echo "Distribution $ID not recognized, exiting ..."
-    exit 1
-    ;;
-esac
+if ! command -v code; then
+    case $ID in
+    fedora)
+        PKG_FMT='rpm'
+        PKG_MGR='dnf'
+        MY_TERM_COMMAND='konsole'
+        MY_FLAGS='-e'
+        ;;
+    linuxmint | ubuntu)
+        PKG_FMT='deb'
+        PKG_MGR='apt'
+        MY_TERM_COMMAND='gnome-terminal'
+        MY_FLAGS='-x'
+        ;;
+    *)
+        echo "Distribution $ID not recognized, exiting ..."
+        exit 1
+        ;;
+    esac
 
-ls "$LOCAL_RES_DIR"code*."$PKG_FMT" &>/dev/null || (
-    read -r -n 1 -s -p "Set firefox about:preferences Downloads to 'Always ask where to save files'
+    ls "$LOCAL_RES_DIR"code*."$PKG_FMT" &>/dev/null || (
+        read -r -n 1 -s -p "Set firefox about:preferences Downloads to 'Always ask where to save files'
 
 Save in ${LOCAL_RES_DIR}
 
     
 Press any key to continue ..." &&
-        "$MY_TERM_COMMAND" "$MY_FLAGS" firefox https://code.visualstudio.com/Download
-)
+            "$MY_TERM_COMMAND" "$MY_FLAGS" firefox https://code.visualstudio.com/Download
+    )
 
-CODE_FILE=$(ls "$LOCAL_RES_DIR"code*."$PKG_FMT") && sudo "$PKG_MGR" install "$CODE_FILE"
-# fi
+    CODE_FILE=$(ls "$LOCAL_RES_DIR"code*."$PKG_FMT") && sudo "$PKG_MGR" install "$CODE_FILE"
+fi
 
 echo " $0 : Exiting ..."
