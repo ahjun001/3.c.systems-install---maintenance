419271ef9880739f14a4ce4a83440b56d8ff05a4
ahjun001 <todo.ubuntu@gmail.com>
2022-05-19 11:52:51 +0800

    new code install commands, as per MS instructions

--- 02_code/02_code.sh
+++ 02_code/02_code.sh
@@ -6,11 +6,10 @@
 # run with arg u  to undo
 
 # launch after install
-[[ -n ${LAUNCH_APP+foo} ]]  || LAUNCH_APP=true
+[[ -n ${LAUNCH_APP+foo} ]] || LAUNCH_APP=true
 
 # info verbose debug trace
 [[ ${MY_TRACE+foo} ]] || MY_TRACE=true
-
 
 # -e to exit on error
 # -u to exit on unset variables
@@ -20,25 +19,37 @@ set -"$MY_ENV"
 
 # set environment: ID, SOURCE_DIR
 # shellcheck source=/dev/null
-[[ -n ${ID+foo} ]]  || . /etc/os-release
+[[ -n ${ID+foo} ]] || . /etc/os-release
 
 # scripts & resources directory
-[[  ${SOURCE_DIR+foo} ]] || SOURCE_DIR="$(pwd)"/
-LOCAL_RES_DIR="$SOURCE_DIR"'Local resources TBU/'
+# [[ ${SOURCE_DIR+foo} ]] || SOURCE_DIR="$(pwd)"/
+# LOCAL_RES_DIR="$SOURCE_DIR"'Local resources TBU/'
 
-if ! command -v code; then
+# if ! command -v code; then
     case $ID in
     fedora)
-        PKG_FMT='rpm'
-        PKG_MGR='dnf'
-        MY_TERM_COMMAND='konsole'
-        MY_FLAGS='-e'
+        # PKG_FMT='rpm'
+        # PKG_MGR='dnf'
+        # MY_TERM_COMMAND='konsole'
+        # MY_FLAGS='-e'
+        sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+        sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+        dnf check-update
+        sudo dnf install code
         ;;
     linuxmint | ubuntu)
-        PKG_FMT='deb'
-        PKG_MGR='apt'
-        MY_TERM_COMMAND='gnome-terminal'
-        MY_FLAGS='-x'
+        # PKG_FMT='deb'
+        # PKG_MGR='apt'
+        # MY_TERM_COMMAND='gnome-terminal'
+        # MY_FLAGS='-x'
+        sudo apt-get install wget gpg
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+        sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+        sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+        rm -f packages.microsoft.gpg
+        sudo apt install apt-transport-https
+        sudo apt update
+        sudo apt install code # or code-insiders
         ;;
     *)
         echo "Distribution $ID not recognized, exiting ..."
@@ -46,17 +57,16 @@ if ! command -v code; then
         ;;
     esac
 
-    ls "$LOCAL_RES_DIR"code*."$PKG_FMT" &>/dev/null || (
-        read -r -n 1 -s -p "Set firefox about:preferences Downloads to 'Always ask where to save files'
+#     ls "$LOCAL_RES_DIR"code*."$PKG_FMT" &>/dev/null || (
+#         read -r -n 1 -s -p "Set firefox about:preferences Downloads to 'Always ask where to save files'
 
-Save in ${LOCAL_RES_DIR}
+# Save in ${LOCAL_RES_DIR}
 
-    
-Press any key to continue ..." &&
-            "$MY_TERM_COMMAND" "$MY_FLAGS" firefox https://code.visualstudio.com/Download
-    )
+# Press any key to continue ..." &&
+#             "$MY_TERM_COMMAND" "$MY_FLAGS" firefox https://code.visualstudio.com/Download
+#     )
 
-    CODE_FILE=$(ls "$LOCAL_RES_DIR"code*."$PKG_FMT") && sudo "$PKG_MGR" install "$CODE_FILE"
-fi
+#     CODE_FILE=$(ls "$LOCAL_RES_DIR"code*."$PKG_FMT") && sudo "$PKG_MGR" install "$CODE_FILE"
+# fi
 
 echo " $0 : Exiting ..."
