c69d942e24e614075506e2259aa2b80cedfc0a35
ahjun001 <todo.ubuntu@gmail.com>
2022-06-30 11:17:04 +0800

    now also reducing *.webp

--- ,width_reduce.sh
+++ ,width_reduce.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1087,SC2046
 cd /home/perubu/.local/share/Anki2/pj/collection.media || exit
-for file in *.jpg *.png *.gif *.jpeg; do
+for file in *.jpg *.png *.gif *.jpeg *.webp; do
   # echo "$file"
   width=$(convert "$file[0]" -format '%w' info:)
   # echo "width =" $width
