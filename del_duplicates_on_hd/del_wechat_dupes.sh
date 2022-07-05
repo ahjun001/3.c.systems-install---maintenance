7938cd28017b07224b792fdc5a740c5ba74ea11b
ahjun001 <todo.ubuntu@gmail.com>
2022-07-05 18:01:16 +0800

    more del

--- del_duplicates_on_hd/del_wechat_dupes.sh
+++ del_duplicates_on_hd/del_wechat_dupes.sh
@@ -1,16 +1,63 @@
 #!/usr/bin/env bash
 
+# remove files that wechat downloads automatically
+# some are good to keep to inspect: *.epub
+# others, starting with names from delete_list can be deleted
+
+set -e
+
 old_wd=$(pwd)
 
-df -h / | tee -a free_space.txt 
 cd '/home/perubu/Documents/WeChat Files/wxid_6761767617821/FileStorage/File' || exit
 
-for i in {0..9}; do
-    find . -name "*($i)*" -exec rm {} \;
+rm -fv free_space.txt
+df -h / | tee -a free_space.txt
+
+# for i in {0..9}; do
+#     find . -name "*($i)*" -exec rm {} \;
+# done
+
+delete_list=(
+    BBC
+    Bloomberg
+    'Business Traveller'
+    Challenges
+    Closer
+    Cuisine
+    Femina
+    FigTV
+    Ici_Paris
+    Journal
+    lefigaro
+    Le_Figaro
+    lemonde
+    LeMonde
+    'Le Monde'
+    'LE MONDE'
+    'Le nouvel'
+    "L'Equipe"
+    lequipe
+    lhumanite
+    "L'Opinion"
+    lesechos
+    'Les Echos'
+    libe
+    Madame
+    Maison
+    Marie_Claire
+    "Mens Health"
+    "Midi Olympique"
+    Paris_Match
+    TV
+)
+
+for name in "${delete_list[@]}"; do
+    # find . -name "$name*.pdf" -exec ls {} \;
+    find . -name "$name*.pdf" -exec rm {} \;
 done
 
-cd "$old_wd" || exit
-
-df -h / | tee -a free_space.txt 
+df -h / | tee -a free_space.txt
 
 cat free_space.txt
+
+cd "$old_wd" || exit
