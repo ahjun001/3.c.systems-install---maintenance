a9c7994cdb256ec18f5bc03ca8c2ed05c862a69d
ahjun001 <todo.ubuntu@gmail.com>
2022-09-21 17:03:08 +0800

    use konsole

--- ,nvim.sh
+++ ,nvim.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1087,SC2046
 # gnome-terminal -x nvim -c 'bro ol'  
-terminator -mx nvim -c 'bro ol'  
+# terminator -mx nvim -c 'bro ol'  
+konsole --fullscreen -e nvim -c 'bro ol'
