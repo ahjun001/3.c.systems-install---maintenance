07745d50c17de49ed28e1b6fffae735498e4bbbd
ahjun001 <todo.ubuntu@gmail.com>
2022-09-22 10:48:28 +0800

    use distro terminal gnome, also can be maximized on CLI

--- ,nvim.sh
+++ ,nvim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1087,SC2046
-# gnome-terminal -x nvim -c 'bro ol'  
+gnome-terminal  --maximize -x nvim -c 'bro ol'  
 # terminator -mx nvim -c 'bro ol'  
-konsole --fullscreen -e nvim -c 'bro ol'
+# konsole --fullscreen -e nvim -c 'bro ol'
